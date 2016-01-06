0af4e1662b6c9f3ed15df6ed69085866e9a41684
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-01-07 02:45:05 +0900

    splitter

--- packet/decode.go
+++ packet/decode.go
@@ -1,24 +1,58 @@
 package packet
 
-import "io"
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+)
 
-// Split splits datagram of a Packet from io.Reader.
-func Split(r io.Reader) ([]byte, error) {
-	// TODO:
-	return nil, nil
+// Reader declares stream which can be decoded as Packets.
+// bufio.Reader is one of satisfied implementations.
+type Reader interface {
+	io.Reader
+	io.ByteReader
 }
 
-// Decode decodes a Packet from datagram.
-func Decode(b []byte) (Packet, error) {
-	// TODO:
-	return nil, nil
-}
-
-// SplitDecode splits datagram from io.Reader and decode it as a Packet.
-func SplitDecode(r io.Reader) (Packet, error) {
+// SplitDecode splits datagram from Reader and decode it as a Packet.
+func SplitDecode(r Reader) (Packet, error) {
 	b, err := Split(r)
 	if err != nil {
 		return nil, err
 	}
 	return Decode(b)
+}
+
+// Split splits datagram of a Packet from Reader.
+func Split(r Reader) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	// read header: message type
+	c, err := r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	err = buf.WriteByte(c)
+	if err != nil {
+		return nil, err
+	}
+	// read length of payload
+	l, err := binary.ReadUvarint(r)
+	if err != nil {
+		return nil, err
+	}
+	// FIXME: should be check l is too long.
+	b := make([]byte, binary.MaxVarintLen32)
+	n := binary.PutUvarint(b, l)
+	buf.Write(b[0:n])
+	// read whole payload to buf.
+	_, err = io.CopyN(buf, r, int64(l))
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// Decode decodes a Packet from datagram.
+func Decode(b []byte) (Packet, error) {
+	// TODO: decode as one of Packets.
+	return nil, nil
 }
