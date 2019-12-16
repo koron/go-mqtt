bfe26eea3a727680bbb8c47966d16c0edf642513
MURAOKA Taro <koron.kaoriya@gmail.com>
2019-12-16 18:13:25 +0900

    make ./cmd/... compilable

--- cmd/mocqlient/mocqlient.go
+++ cmd/mocqlient/mocqlient.go
@@ -1,52 +1,12 @@
 package main
 
-import (
-	"log"
-
-	"github.com/surgemq/message"
-	"github.com/surgemq/surgemq/service"
-)
-
-func pub(c *service.Client, msg *message.PublishMessage) error {
-	log.Printf("on publish: topic=%s payload=%s\n", string(msg.Topic()), string(msg.Payload()))
-	pub := message.NewPublishMessage()
-	pub.SetTopic([]byte("a/b/c/response"))
-	pub.SetQoS(0)
-	pub.SetPayload([]byte("Hi MQTT Server"))
-	return c.Publish(pub, nil)
-}
-
 func main() {
-	c := &service.Client{}
+	// TODO: write a client with go-mqtt/client, it should do...
+	// 1. connect to MQTT server
+	// 2. subscribe "#" topic.
+	// 3. publish a message "Hi MQTT Server" to "a/b/c/response" topic
+	// 4. log messages on each phases
 
-	msgConn := message.NewConnectMessage()
-	msgConn.SetVersion(3)
-	msgConn.SetCleanSession(true)
-	msgConn.SetClientId([]byte("01234567"))
-	msgConn.SetKeepAlive(300)
-
-	if err := c.Connect("tcp://127.0.0.1:1883", msgConn); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("connected")
-
-	msgSub := message.NewSubscribeMessage()
-	msgSub.AddTopic([]byte("#"), 0)
-	onPub := func(msg *message.PublishMessage) error {
-		return pub(c, msg)
-	}
-	onComp := func(msg, ack message.Message, err error) error {
-		log.Printf("on complete subscribe: msg=%#v ack=%#v err=%v\n", msg, ack, err)
-		return err
-	}
-	if err := c.Subscribe(msgSub, onComp, onPub); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("subscribed")
-
-	//c.Disconnect()
-	//log.Println("disconnected")
-
-	done := make(chan bool)
+	done := make(chan struct{})
 	<-done
 }
